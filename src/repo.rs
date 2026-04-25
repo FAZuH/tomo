fde9cc00367949fe57b9c916e185e36b0314e9c0
FAZuH <mail@fazuh.com>
2026-04-25 19:04:03 +0700

    feat(db): add basic repo & sqlite structure

--- src/repo.rs
+++ src/repo.rs
@@ -1,7 +1,17 @@
 use diesel_migrations::EmbeddedMigrations;
 use diesel_migrations::embed_migrations;
 
+pub mod error;
 pub mod model;
 pub mod schema;
+pub mod sqlite;
+pub mod traits;
+
+pub use error::RepoError;
+pub use traits::ProjectRepository;
+pub use traits::Repositories;
+pub use traits::SessionRepository;
+pub use traits::TagRepository;
+pub use traits::TaskRepository;
 
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
