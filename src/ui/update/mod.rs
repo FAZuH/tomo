96466719bbcb2cfb9f1c55b9bc75c4c1efb9f5ba
FAZuH <mail@fazuh.com>
2026-05-01 00:57:47 +0700

    refactor(ui): correct module name typo

--- src/ui/update/mod.rs
+++ src/ui/update/mod.rs
@@ -1,6 +1,6 @@
 pub mod config;
-pub mod pomor;
+pub mod pomo;
 pub use config::ConfigCmd;
 pub use config::ConfigMsg;
-pub use pomor::PomodoroCmd;
-pub use pomor::PomodoroMsg;
+pub use pomo::PomodoroCmd;
+pub use pomo::PomodoroMsg;
